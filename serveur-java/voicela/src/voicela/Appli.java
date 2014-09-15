3e220ba62e7cc3623ef7fd52ce6d0054b45e7b50
maxencebeno <maxence.beno@gmail.com>
2014-09-15 17:28:14 +0200

    Connexion!

--- serveur-java/voicela/src/voicela/Appli.java
+++ serveur-java/voicela/src/voicela/Appli.java
@@ -14,7 +14,7 @@ import java.util.*;
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import static voicela.Connexion.lister;
+import static voicela.Connexion.conn;
 /**
  *
  * @author Vincent
@@ -24,8 +24,49 @@ public class Appli {
     /**
      * @param args the command line arguments
      */
+    
+    public static java.sql.Connection conn;
+    
     public static void main(String[] args) {
-        lister();
-        VIP vip = new VIP();
-    }
+        try {
+            System.out.println( "Chargement du driver..." );
+            Class.forName( "com.mysql.jdbc.Driver" );
+            System.out.println( "Driver chargé !" );
+        } catch ( ClassNotFoundException e ) {
+            System.out.println( "Erreur lors du chargement : le driver n'a pas été trouvé dans le classpath ! <br/>"
+                    + e.getMessage() );
+        }
+        try{
+
+            // parametres de connexion
+            String url="jdbc:mysql://iutdoua-webetu.univ-lyon1.fr/";
+            String login="p1201896";
+            String pass="169657";
+
+            // connexion
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            conn = java.sql.DriverManager.getConnection(url+"?user="+login+"&password="+pass);
+            System.out.println("\n connection reussie \n");
+			
+	}
+
+	catch(Exception e){
+            System.out.println("\n Connection ratee: "+ e);
+            System.exit(-1);
+	}
+		try {
+            // la requete
+			String requete = "SELECT * FROM vip";
+			// execution de la requete
+			Statement stat=conn.createStatement();
+			ResultSet rset = stat.executeQuery(requete);
+			// affichage du rÈsultat
+			while(rset.next())
+				System.out.println("\t"+rset.getString("nom_vip"));
+			rset.close();
+        } catch (Exception ex) {
+            System.out.println("\n Probleme dans la requete listeEmp: " + ex.getMessage());           
+        }
+	}
+    
 }
