8c444679d67c7911713c3c166cd2d45f1a6a0ed1
EtienneTr <tienou42@gmail.com>
2014-09-16 11:17:32 +0200

    script v2

--- script_sql/script.sql
+++ script_sql/script.sql
@@ -5,13 +5,13 @@ CREATE TABLE vip (
 	nom_vip varchar2(255),
 	prenom_usuel_vip varchar2(255),
 	prenoms_vip varchar2(255),
+	nationalite_vip varchar2(255),
 	civilite_vip varchar2(255),
 	date_naissance_vip date,
 	lieu_naissance_vip varchar2(255),
-	sexe_vip varchar2(255),
 	statut_vip varchar2(255),
 	constraint pk_vip primary key (num_vip),
-	constraint ck_vip_statut_vip check (statut_vip IN ('Acteur', 'Réalisateur', 'Rien'))
+	constraint ck_vip_statut_vip check (statut_vip IN ('Acteur', 'Realisateur', 'null'))
 );
 
 CREATE TABLE photos (
@@ -19,7 +19,7 @@ CREATE TABLE photos (
 	num_vip number(10),
 	url_photo varchar2(255),
 	date_ajout_photo date,
-	constraint pk_photo primary key (id_photo)
+	constraint pk_photos primary key (id_photo)
 );
 
 
@@ -32,40 +32,25 @@ CREATE TABLE films (
 	constraint pk_films primary key (id_film)
 );
 
-CREATE TABLE acteur (
-	num_vip number(10) not null,
-	id_film number(10) not null,
-	constraint pk_acteur primary key (num_vip, id_film)
-);
-
-CREATE TABLE realisateur (
-	num_vip number(10) not null,
-	id_film number(10) not null,
-	constraint pk_realisateur primary key (num_vip, id_film)
-);
-
 CREATE TABLE mariage (
-	num_vip1 number(10) not null,
-	num_vip2 number(10) not null,
+	id_mariage number(10),
+	lieu_mariage varchar2(255),
 	date_mariage date,
-	constraint pk_mariage primary key (num_vip1, num_vip2)
-);
-
-CREATE TABLE divorce (
-	num_vip1 number(10) not null,
-	num_vip2 number(10) not null,
+	divorce boolean,
 	date_divorce date,
-	constraint pk_divorce primary key (num_vip1, num_vip2)
+	constraint pk_mariage primary key (id_mariage)
 );
 
-ALTER TABLE photographie add constraint fk_photose_vip foreign key (num_vip) references vip (num_vip); 
-alter table acteur add constraint fk_acteur_id_film foreign key (id_film) references films(id_film);
-alter table acteur add constraint fk_acteur_num_vip foreign key (num_vip) references vip(num_vip);
-alter table realisateur add constraint fk_realisateur_id_film foreign key (id_film) references films(id_film);
-alter table realisateur add constraint fk_realisateur_num_vip foreign key (num_vip) references vip(num_vip);
-alter table mariage add constraint fk_mariage_num_vip1 foreign key (num_vip1) references vip (num_vip);
-alter table mariage add constraint fk_mariage_num_vip2 foreign key (num_vip2) references vip (num_vip);
-alter table divorce add constraint fk_mariage_num_vip1 foreign key (num_vip1) references vip (num_vip);
-alter table divorce add constraint fk_mariage_num_vip2 foreign key (num_vip2) references vip (num_vip);
+create table maries (
+	id_mariage number(10),
+	num_vip number(10)
+	constraint pk_maries primary key (id_mariage, num_vip)
+);
+
+
+ALTER TABLE photos add constraint fk_photos_vip foreign key (num_vip) references vip (num_vip); 
+
+alter table maries add constraint fk_maries_num_vip foreign key (num_vip) references vip (num_vip);
+alter table maries add constraint fk_maries_id_mariage foreign key (id_mariage) rgyeferences mariage (id_mariage);
 
 
