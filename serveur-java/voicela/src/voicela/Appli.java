b16d831d703676c2a035dc8356c13a04b85d388a
maxencebeno <maxence.beno@gmail.com>
2014-09-16 10:48:58 +0200

    Connexion réussi

--- serveur-java/voicela/src/voicela/Appli.java
+++ serveur-java/voicela/src/voicela/Appli.java
@@ -28,48 +28,37 @@ public class Appli {
     public static java.sql.Connection conn;
     
     public static void main(String[] args) {
-        Statement statement = null;
-        ResultSet resultat = null;
-        Connection connexion = null;
-        try {
-            System.out.println( "Chargement du driver..." );
-            Class.forName( "com.mysql.jdbc.Driver" );
-            System.out.println( "Driver chargé !" );
-        } catch ( ClassNotFoundException e ) {
-            System.out.println( "Erreur lors du chargement : le driver n'a pas été trouvé dans le classpath ! <br/>"
-                    + e.getMessage() );
-        }
         try{
-
-            // parametres de connexion
+            
+        // parametres de connexion
             String url="jdbc:mysql://iutdoua-webetu.univ-lyon1.fr/p1200270";
             String login="p1200270";
             String pass="168511";
 
-            // connexion
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-            conn = java.sql.DriverManager.getConnection(url+"?user="+login+"&password="+pass);
-            System.out.println("\n connection reussie \n");
-			
-	}
+        // connexion
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        conn = java.sql.DriverManager.getConnection(url+"?user="+login+"&password="+pass);
+        System.out.println("\n connection reussie \n");
 
-	catch(Exception e){
+        }
+
+    catch(Exception e){
             System.out.println("\n Connection ratee: "+ e);
             System.exit(-1);
-	}
-	try {
-            System.out.println( "Requête préparée créée !" );
-            String requete = "SELECT * FROM images;";
-            resultat = statement.executeQuery(requete);
-            System.out.println( "Requête \"SELECT * FROM photographie;\" effectuée !" );
-
-            /* Récupération des données du résultat de la requête de lecture */
-            while ( resultat.next() ) {
-                /* Formatage des données pour affichage dans la JSP finale. */
-                System.out.println( resultat.getInt("id_image") );
-            }
-	} catch (SQLException sqle) {
-            System.out.println("Erreur" + sqle);
+    }  
+        try {
+            // la requete
+            String requete = "SELECT * FROM images";
+            // execution de la requete
+            Statement stat=conn.createStatement();
+            ResultSet rset = stat.executeQuery(requete);
+            // affichage du résultat
+            while(rset.next())
+            System.out.println(rset.getInt("id_image"));
+            rset.close();
+        } catch (Exception ex) {
+            System.out.println("\n Probleme dans la requete" + ex.getMessage());           
         }
+        
     }
 }
