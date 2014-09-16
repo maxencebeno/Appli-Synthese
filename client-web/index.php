350238320ffb0d899f3016a77423e5ceaf795e1c
vincentmoral <vincent.moral69@yahoo.fr>
2014-09-16 14:03:40 +0200

    Page d'accueil du site

--- client-web/index.php
+++ client-web/index.php
@@ -2,12 +2,32 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <link rel="stylesheet" href="assets/style_index.css" />
+        <link rel="stylesheet" type="text/css" href="assets/css/universal.css" />
+        <link rel="stylesheet" type="text/css" href="assets/css/style_index.css" />
         <title>Voicela</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
     </head>
 
     <body>
-        <h1>Voicela</h1>
-        <?php echo "dfsdfsf"; ?>
+        <header class="general_header">Voicela</header>
+        <div class="full_wrapper">
+            <section id="section_one">
+                <div class="section_content">
+                    <h1>Suivez au plus près l'actualité people.</h1>
+                </div>
+            </section>
+            <section style="height: 800px;">
+
+            </section>
+            
+
+        </div>
+        <script src="assets/js/core.js"></script>
+        <script src="assets/js/parallax.js"></script>
+        <script type="text/javascript"> 
+            $(document).ready(function(){       
+                $('#section_one').parallax("center", 0, 0.1, true);
+            });
+        </script>
     </body>
 </html>
