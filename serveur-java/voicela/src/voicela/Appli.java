063372c438b54609574ad19611230f7aa5c96219
vincentmoral <vincent.moral69@yahoo.fr>
2014-09-16 11:09:51 +0200

    Dimensions interface

--- serveur-java/voicela/src/voicela/Appli.java
+++ serveur-java/voicela/src/voicela/Appli.java
@@ -45,11 +45,11 @@ public class Appli extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 962, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 600, Short.MAX_VALUE)
         );
 
         pack();
