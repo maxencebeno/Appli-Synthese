4a43d94f87b832ef1ff3e3a4dc9b69b324a7d628
maxencebeno <maxence.beno@gmail.com>
2014-09-16 11:06:57 +0200

    petit changement

--- serveur-java/voicela/src/voicela/Appli.java
+++ serveur-java/voicela/src/voicela/Appli.java
@@ -39,6 +39,7 @@ public class Appli extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Voicela");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
