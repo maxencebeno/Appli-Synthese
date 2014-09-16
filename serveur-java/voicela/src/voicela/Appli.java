153de675658b1bbbd5ccc10d0ae601ea24a9d880
maxencebeno <maxence.beno@gmail.com>
2014-09-16 10:53:40 +0200

    Classe connexion

--- serveur-java/voicela/src/voicela/Appli.java
+++ serveur-java/voicela/src/voicela/Appli.java
@@ -28,37 +28,6 @@ public class Appli {
     public static java.sql.Connection conn;
     
     public static void main(String[] args) {
-        try{
-            
-        // parametres de connexion
-            String url="jdbc:mysql://iutdoua-webetu.univ-lyon1.fr/p1200270";
-            String login="p1200270";
-            String pass="168511";
-
-        // connexion
-        Class.forName("com.mysql.jdbc.Driver").newInstance();
-        conn = java.sql.DriverManager.getConnection(url+"?user="+login+"&password="+pass);
-        System.out.println("\n connection reussie \n");
-
-        }
-
-    catch(Exception e){
-            System.out.println("\n Connection ratee: "+ e);
-            System.exit(-1);
-    }  
-        try {
-            // la requete
-            String requete = "SELECT * FROM images";
-            // execution de la requete
-            Statement stat=conn.createStatement();
-            ResultSet rset = stat.executeQuery(requete);
-            // affichage du résultat
-            while(rset.next())
-            System.out.println(rset.getInt("id_image"));
-            rset.close();
-        } catch (Exception ex) {
-            System.out.println("\n Probleme dans la requete" + ex.getMessage());           
-        }
-        
+        Connexion.Connecter(args);
     }
 }
