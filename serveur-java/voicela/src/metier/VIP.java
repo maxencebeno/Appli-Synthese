9a8c17a29d67934ae0dc54e765541fd7ba11dd03
vincentmoral <vincent.moral69@yahoo.fr>
2014-09-29 16:53:01 +0200

    Mariage + Divorce

--- serveur-java/voicela/src/metier/VIP.java
+++ serveur-java/voicela/src/metier/VIP.java
@@ -141,16 +141,7 @@ public class VIP {
     
      
     // Méthodes
-    public void seMarier(VIP vip, String lieuMariage, Date dateMariage){
-        // Instanciation du mariage
-        /*Mariage mariage = new Mariage();
-        mariage.setNumMariage(1);
-        mariage.setMarie1(this.nom);
-        mariage.setMarie2(vip.getNom());
-        mariage.setLieuMariage(lieuMariage);
-        mariage.setDivorce(false);
-        mariage.setDateMariage(dateMariage);*/
-        
+    public void seMarier(VIP vip1, VIP vip2, String lieuMariage, Date dateMariage){     
         // Insertion du mariage en base de données
         PreparedStatement ps = null;
         ResultSet rs = null;
@@ -160,17 +151,19 @@ public class VIP {
             connection = cnx.Connecter();
             
             //----------------------------------------------------------------
-            // PREMIERE INSERTION
+            // INSERTION
             //----------------------------------------------------------------
             // Préparation de la date pour conversion date java -> date SQL
             java.util.Date utilDate = dateMariage;
             java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
             // Requête d'insertion
             ps = connection.prepareStatement("INSERT INTO mariage (num_vip1, num_vip2, lieu_mariage, date_mariage, divorce, date_divorce) VALUES (?, ?, ?, ?, ? , ?)");         
-            ps.setString(1, lieuMariage);
-            ps.setDate(2, sqlDate);
-            ps.setBoolean(3, false);
-            ps.setDate(4, null);
+            ps.setInt(1, vip1.getId());
+            ps.setInt(2, vip2.getId());
+            ps.setString(3, lieuMariage);
+            ps.setDate(4, sqlDate);
+            ps.setBoolean(5, false);
+            ps.setDate(6, null);
             // On valide la première insertion
             ps.executeUpdate();
         }catch(Exception e){
@@ -186,7 +179,39 @@ public class VIP {
         }
     }
     
-    public void divorcer(VIP vip){
-        
+    public void divorcer(Mariage mariage){
+        // Insertion du mariage en base de données
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Connection connection = null;
+        try {
+            Connexion cnx = new Connexion();
+            connection = cnx.Connecter();
+            
+            //----------------------------------------------------------------
+            // INSERTION
+            //----------------------------------------------------------------
+            // Préparation de la date pour conversion date java -> date SQL
+            java.util.Date utilDate = new Date();
+            java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+            
+            // Requête d'update pour passer à divorcé et mettre la date de divorce
+            ps = connection.prepareStatement("UPDATE mariage SET divorce = 1 AND date_divorce = ? WHERE id_mariage = ? ");         
+            ps.setInt(1, mariage.getNumMariage());
+            ps.setDate(2, sqlDate);
+            
+            // On valide le divorce
+            ps.executeUpdate();
+        }catch(Exception e){
+            //throw e;
+        }finally{
+            try{
+                if(rs != null) rs.close();
+                if(ps != null) ps.close();
+                if(connection != null) connection.close();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
