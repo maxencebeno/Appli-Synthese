447b7af0629da049dcc1537e6f50e171ab9ea21d
vincentmoral <vincent.moral69@yahoo.fr>
2014-09-30 11:58:58 +0200

    Save

---
+++ client-web/controleur/bio/controleur_bio.php
@@ -0,0 +1,5 @@
+<?php
+ //On affiche la page (vue)
+include_once('vue/bio/vue_bio.php');
+
+?>
--- client-web/vue/includes/header.php
+++ client-web/vue/includes/header.php
@@ -1,1 +1,10 @@
-<header class="general_header"><img src="assets/images/index/voicela.png" alt="Voicela" /></header>
+<header class="general_header">
+    <div id="header_container">
+        <ul>
+            <li>Voicela</li>
+            <li>Biographies</li>
+            <li>Photos</li>
+            <li>Autre</li>
+        </ul>
+    </div>
+</header>
