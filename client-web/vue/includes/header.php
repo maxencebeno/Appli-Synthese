f7b266ca3d7488b461befd837a81fa776e3d3cbe
vincentmoral <vincent.moral69@yahoo.fr>
2014-10-06 15:03:12 +0200

    Améliorations headr

--- client-web/vue/includes/header.php
+++ client-web/vue/includes/header.php
@@ -1,10 +1,10 @@
 <header class="general_header">
     <div id="header_container">
         <ul>
-            <li><span class="glyphicon glyphicon-home"></span><a href="index.php">Voicela</a></li>
-            <li><a href="profil.php">Biographies</a></li>
-            <li><a href="picture.php">Photos</a></li>
-            <li><a href="other.php">Autre</a></li>
+            <li><a href="index.php" class="glyphicon glyphicon-home"><span style="margin-left: 10px">Voicela</span></a></li>
+            <li><a href="profil.php" class="glyphicon glyphicon-user"><span style="margin-left: 10px">Profil</span></a></li>
+            <li><a href="picture.php" class="glyphicon glyphicon-picture"><span style="margin-left: 10px">Photos</span></a></li>
+            <li><a href="other.php" class="glyphicon glyphicon-asterisk"><span style="margin-left: 10px">Autre</span></a></li>
         </ul>
     </div>
 </header>
