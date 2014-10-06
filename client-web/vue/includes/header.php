6e77b983d24dd1d063325eb47aebca1be4b0d5cd
vincentmoral <vincent.moral69@yahoo.fr>
2014-10-06 14:50:08 +0200

    Update importante

--- client-web/vue/includes/header.php
+++ client-web/vue/includes/header.php
@@ -1,10 +1,10 @@
 <header class="general_header">
     <div id="header_container">
         <ul>
-            <li>Voicela</li>
-            <li>Biographies</li>
-            <li>Photos</li>
-            <li>Autre</li>
+            <li><span class="glyphicon glyphicon-home"></span><a href="index.php">Voicela</a></li>
+            <li><a href="profil.php">Biographies</a></li>
+            <li><a href="picture.php">Photos</a></li>
+            <li><a href="other.php">Autre</a></li>
         </ul>
     </div>
 </header>
