99767b72df11942036cde4a1fd315cf6ce789198
vincentmoral <vincent.moral69@yahoo.fr>
2014-10-06 16:20:07 +0200

    Commit

--- client-web/modele/connexion.php
+++ client-web/modele/connexion.php
@@ -1,22 +1,22 @@
 <?php 
 function connexion(){	
-		$host='iutdoua-webetu.univ-lyon1.fr';
-		$user='p1201896';
-		$password='169657';
-		$basename='p1201896';
+	$host='iutdoua-webetu.univ-lyon1.fr';
+	$user='p1201896';
+	$password='169657';
+	$basename='p1201896';
 		
-		try
-		{
-    // On se connecte à MySQL
-  		  $bdd = new PDO('mysql:host='.$host.';dbname='.$basename, $user, $password);
-		}
-		catch(Exception $e)
-		{
-    // En cas d'erreur, on affiche un message et on arrête tout
+	try
+	{
+        // On se connecte à MySQL
+  		$bdd = new PDO('mysql:host='.$host.';dbname='.$basename, $user, $password);
+	}
+	catch(Exception $e)
+	{
+        // En cas d'erreur, on affiche un message et on arrête tout
         //die('Erreur : '.$e->getMessage());
 		echo"erreur";
-		}
+	}
 
-		return $bdd;
+	return $bdd;
 }
 ?>
