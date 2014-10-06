f0d9c68899b9488b7c17ae57c6b3613b4efa0f01
EtienneTr <tienou42@gmail.com>
2014-10-06 15:25:26 +0200

    commit profil :dancer:

--- client-web/controleur/profile/controleur_profile.php
+++ client-web/controleur/profile/controleur_profile.php
@@ -1,4 +1,5 @@
 <?php
-include_once("modele\connexion.php");
-include_once("modele\profile\modele_profile.php");
-include_once("vue\profile\vue_profile.php");
+include_once("modele/connexion.php");
+include_once("modele/profile/modele_profile.php");
+include_once("vue/profile/vue_profile.php");
+?>
