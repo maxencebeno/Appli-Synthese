0e23d554f2d897f2c49c3aabdc829a4ca4c14fea
EtienneTr <tienou42@gmail.com>
2014-10-20 16:51:13 +0200

    suite #3

--- client-web/vue/film/vue_film.php
+++ client-web/vue/film/vue_film.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <link rel="stylesheet" type="text/css" href="assets/css/universal.css" />
-        <link rel="stylesheet" type="text/css" href="assets/css/profile/style_profile.css" />
+        <link rel="stylesheet" type="text/css" href="assets/css/film/style_film.css" />
         <link rel="stylesheet" type="text/css" href="assets/css/glyphicon.css" />
         <title>Films - Voicela</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
@@ -14,7 +14,7 @@
         <div class="full_wrapper">
         <h1>Films</h1>
 
-        <div class="film_container">
+        <div class="film_wrapper">
 
         <?php
         while($films= $reqFilms->fetch()){
@@ -25,14 +25,15 @@
             $num_visa = $films["num_visa"];
             $photo = $films["url_photo"];
 
-            
+            echo'<div class="films_container">';
+
             echo '<div class="pres">';
-            echo '<img src="files/films/'.$photo.'"/>';
-            echo '<span class="info">
+            echo '<img class="img_container" src="files/films/'.$photo.'"/><br/>';
+            echo '<p class="info">
                     '.$titre_film.'<br/>
-                    '.date('Y', $date_sortie).'
+                    '.$date_sortie.'
                      - '.$genre.'
-                 </span>';
+                 </p>';
 
 
             echo '</div>';
@@ -42,6 +43,7 @@
 
 
 
+            echo '</div>';
             echo '</div>';
             
 
