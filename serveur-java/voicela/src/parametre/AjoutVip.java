091429196088f7bf2cb6593a1ff28e6139e917a5
maxencebeno <maxence.beno@gmail.com>
2014-10-29 14:31:59 +0100

    Bug ajout vip

--- serveur-java/voicela/src/parametre/AjoutVip.java
+++ serveur-java/voicela/src/parametre/AjoutVip.java
@@ -345,12 +345,12 @@ public class AjoutVip extends javax.swing.JDialog {
         if (dateNaissanceVIP.compareTo("") == 0 || nationaliteVIP.compareTo("") == 0 || prenoms.compareTo("") == 0 || prenomsUsage.compareTo("") == 0 || nomVIP.compareTo("") == 0 || lieuNaissanceVIP.compareTo("") == 0) {
             javax.swing.JOptionPane.showMessageDialog(
                     this,
-                    "Vous n'avez rempli aucun champs",
+                    "Certains champs ne sont pas remplis",
                     "Erreur",
                     javax.swing.JOptionPane.ERROR_MESSAGE
             );
             ajoutVip = false;
-        } else if (dateNaissanceVIP.compareTo("") == 0 && nationaliteVIP.compareTo("") == 0 && prenoms.compareTo("") == 0 && prenomsUsage.compareTo("") == 0 && nomVIP.compareTo("") == 0 && lieuNaissanceVIP.compareTo("") == 0) {
+        } /*else if (dateNaissanceVIP.compareTo("") == 0 && nationaliteVIP.compareTo("") == 0 && prenoms.compareTo("") == 0 && prenomsUsage.compareTo("") == 0 && nomVIP.compareTo("") == 0 && lieuNaissanceVIP.compareTo("") == 0) {
             javax.swing.JOptionPane.showMessageDialog(
                     this,
                     "Vous n'avez rempli aucun champs",
@@ -358,7 +358,7 @@ public class AjoutVip extends javax.swing.JDialog {
                     javax.swing.JOptionPane.ERROR_MESSAGE
             );
             ajoutVip = false;
-        } else {
+        }*/ else {
             if (dateNaissanceVIP.compareTo("") == 0) {
                 javax.swing.JOptionPane.showMessageDialog(
                         this,
